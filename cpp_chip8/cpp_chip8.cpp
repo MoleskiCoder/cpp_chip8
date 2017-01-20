00c7b1e0cbc1cfb1e3c80d0d673f3b41b79cd55d
Adrian Conlon <adrian.conlon@gmail.com>
2017-01-20 10:44:28 +0000

    Much better game loop: CPU drops to zero, VSYNC used.
    Signed-off-by: Adrian Conlon <adrian.conlon@gmail.com>

--- cpp_chip8/cpp_chip8.cpp
+++ cpp_chip8/cpp_chip8.cpp
@@ -3,116 +3,44 @@
 
 #include "stdafx.h"
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-
-//Starts up SDL and creates window
-bool init();
-
-//Loads media
-bool loadMedia();
-
-//Frees media and shuts down SDL
-void close();
-
-//The window we'll be rendering to
-SDL_Window* gWindow = NULL;
-
-//The surface contained by the window
-SDL_Surface* gScreenSurface = NULL;
-
-//The image we will load and show on the screen
-SDL_Surface* gXOut = NULL;
-
-bool init() {
-	//Initialization flag
-	bool success = true;
-
-	// SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
-
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-		success = false;
-	} else {
-		//Create window
-		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (gWindow == NULL) {
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-			success = false;
-		} else {
-			//Get window surface
-			gScreenSurface = SDL_GetWindowSurface(gWindow);
-		}
-	}
-
-	return success;
-}
-
-bool loadMedia() {
-	//Loading success flag
-	bool success = true;
-
-	//Load splash image
-	gXOut = SDL_LoadBMP("../resources/x.bmp");
-	if (gXOut == NULL) {
-		printf("Unable to load image %s! SDL Error: %s\n", "../resources/x.bmp", SDL_GetError());
-		success = false;
-	}
-
-	return success;
-}
-
-void close() {
-	//Deallocate surface
-	SDL_FreeSurface(gXOut);
-	gXOut = NULL;
-
-	//Destroy window
-	SDL_DestroyWindow(gWindow);
-	gWindow = NULL;
-
-	//Quit SDL subsystems
-	SDL_Quit();
-}
-
 int main(int argc, char* args[]) {
-	//Start up SDL and create window
-	if (!init()) {
-		printf("Failed to initialize!\n");
-	} else {
-		//Load media
-		if (!loadMedia()) {
-			printf("Failed to load media!\n");
-		} else {
-			//Main loop flag
-			bool quit = false;
 
-			//Event handler
-			SDL_Event e;
+	SDL_Window *win = NULL;
+	SDL_Renderer *renderer = NULL;
+	SDL_Texture *bitmapTex = NULL;
+	SDL_Surface *bitmapSurface = NULL;
 
-			//While application is running
-			while (!quit) {
-				//Handle events on queue
-				while (SDL_PollEvent(&e) != 0) {
-					//User requests quit
-					if (e.type == SDL_QUIT) {
-						quit = true;
-					}
-				}
+	int posX = 100, posY = 100, width = 320, height = 240;
 
-				//Apply the image
-				SDL_BlitSurface(gXOut, NULL, gScreenSurface, NULL);
+	SDL_Init(SDL_INIT_VIDEO);
 
-				//Update the surface
-				SDL_UpdateWindowSurface(gWindow);
+	win = SDL_CreateWindow("Hello World", posX, posY, width, height, 0);
+
+	renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+	bitmapSurface = SDL_LoadBMP("../resources/x.bmp");
+
+	bitmapTex = SDL_CreateTextureFromSurface(renderer, bitmapSurface);
+	SDL_FreeSurface(bitmapSurface);
+
+	while (1) {
+		SDL_Event e;
+		if (SDL_PollEvent(&e)) {
+			if (e.type == SDL_QUIT) {
+				break;
 			}
 		}
+
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, bitmapTex, NULL, NULL);
+		SDL_RenderPresent(renderer);
 	}
 
-	//Free resources and close SDL
-	close();
+	SDL_DestroyTexture(bitmapTex);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(win);
+
+	SDL_Quit();
 
 	return 0;
 }
