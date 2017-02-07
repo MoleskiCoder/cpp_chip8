9bf2c049b791ee1a3d8ab180ac6126c58f4afcde
Adrian Conlon <adrian.conlon@gmail.com>
2017-02-07 19:11:51 +0000

    Tidy the options code a little.
    Signed-off-by: Adrian Conlon <adrian.conlon@gmail.com>

--- cpp_chip8/cpp_chip8.cpp
+++ cpp_chip8/cpp_chip8.cpp
@@ -6,52 +6,34 @@
 
 #include <boost/program_options.hpp>
 
-static boost::program_options::variables_map processCommandLine(int argc, char* argv[]) {
+namespace po = boost::program_options;
 
-	boost::program_options::options_description poOptionsDescription("Allowed options");
+static po::variables_map processCommandLine(int argc, char* argv[]) {
+
+	po::options_description poOptionsDescription("Allowed options");
 
 	poOptionsDescription.add_options()
-
-		("processor-type",
-			boost::program_options::value<std::string>()->default_value("schip"),
-			"Processor type.  Can be one of chip, schip or xochip")
-
-		("allow-misaligned-opcodes",
-			boost::program_options::value<bool>(),
-			"Allow instuctions to be loaded from odd addresses")
-
-		("rom",
-			boost::program_options::value<std::string>()->required(),
-			"ROM to use")
-
-		("graphics-count-row-hits",
-			boost::program_options::value<bool>(),
-			"Graphics: count row hits")
-
-		("graphics-count-exceeded-rows",
-			boost::program_options::value<bool>(),
-			"Graphics: count exceeded rows")
-
-		("graphics-clip",
-			boost::program_options::value<bool>(),
-			"Graphics: clip")
-
-		("cycles-per-frame",
-			boost::program_options::value<int>(),
-			"cycles per frame")
+		("processor-type",				po::value<std::string>()->default_value("schip"),		"Processor type.  Can be one of chip, schip or xochip")
+		("allow-misaligned-opcodes",	po::value<bool>(),										"Allow instuctions to be loaded from odd addresses")
+		("rom",							po::value<std::string>()->required(),					"ROM to use")
+		("graphics-count-row-hits",		po::value<bool>(),										"Graphics: count row hits")
+		("graphics-count-exceeded-rows",po::value<bool>(),										"Graphics: count exceeded rows")
+		("graphics-clip",				po::value<bool>(),										"Graphics: clip")
+		("cycles-per-frame",			po::value<int>(),										"cycles per frame")
 	;
 
-	boost::program_options::positional_options_description poPositionalOptions;
+	po::positional_options_description poPositionalOptions;
 	poPositionalOptions.add("rom", 1);
 
-	boost::program_options::command_line_parser poCommandLineParser(argc, argv);
+	po::command_line_parser poCommandLineParser(argc, argv);
 
-	boost::program_options::variables_map options;
-	boost::program_options::store(poCommandLineParser.options(poOptionsDescription).positional(poPositionalOptions).run(), options);
+	po::variables_map options;
+	po::store(poCommandLineParser.options(poOptionsDescription).positional(poPositionalOptions).run(), options);
 
 	try {
-		boost::program_options::notify(options);
+		po::notify(options);
 	} catch (std::exception& error) {
+		std::cerr << "Error: " << error.what() << std::endl;
 		options.clear();
 	}
 
@@ -60,10 +42,8 @@ static boost::program_options::variables_map processCommandLine(int argc, char* 
 
 int main(int argc, char* argv[]) {
 
-
 	auto options = processCommandLine(argc, argv);
 	if (options.empty()) {
-		std::cerr << "Error: missing path to ROM" << std::endl;
 		return 1;
 	}
 
@@ -100,13 +80,10 @@ int main(int argc, char* argv[]) {
 		configuration.setCyclesPerFrame(cyclesPerFrameOption.as<int>());
 	}
 
-	auto romOption = options["rom"];
-	if (romOption.empty()) {
-		return 1;	// no ROM offered
-	}
-	auto game = romOption.as<std::string>();
-
 	std::shared_ptr<Chip8> processor(Controller::buildProcessor(configuration));
+
+	// Because this option is required, we don't need to check whether it's there or not.
+	auto game = options["rom"].as<std::string>();
 	Controller controller(processor.get(), game);
 
 	controller.loadContent();
